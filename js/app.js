6a85ddf6b6f6e723d24d8077c1a2f02587cf8f0e
Amr Aly Elzafarani <amrelza3frany@gmail.com>
2018-09-02 20:34:33 +0200

    add readme and update app.js

--- js/app.js
+++ js/app.js
@@ -1,30 +1,30 @@
 // Enemies our player must avoid
+'use strict';
+
 class Enemy {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
     constructor(x, y, speed) {
-        // Variables applied to each of our instances go here,
-        // we've provided one for you to get started
-        this.x = x;
-        this.y = y;
-        this.speed = speed;
-
-        // The image/sprite for our enemies, this uses
-        // a helper we've provided to easily load images
-        this.sprite = 'images/enemy-bug.png';
-    }
-
-    // Update the enemy's position, required method for game
-    // Parameter: dt, a time delta between ticks
+            this.x = x;
+            this.y = y;
+            this.speed = speed;
+            // The image/sprite for our enemies, this uses
+            // a helper we've provided to easily load images
+            this.sprite = 'images/enemy-bug.png';
+        }
+        // Update the enemy's position, required method for game
+        // Parameter: dt, a time delta between ticks
     update(dt) {
         // You should multiply any movement by the dt parameter
         // which will ensure the game runs at the same speed for
         // all computers.
         this.x += this.speed * dt;
-        // when off canvas, reset position of enemy to move across again
+        // reset position of enemy to move across again
         if (this.x > 505) {
             this.x = 0;
         }
-
-        // Check for collision between player and enemies
+        // Check
+        // for collision between player and enemies
         if (player.x < this.x + 60 &&
             player.x + 37 > this.x &&
             player.y < this.y + 25 &&
@@ -32,18 +32,17 @@ class Enemy {
             player.x = 200;
             player.y = 380;
         }
-
-        // Draw the enemy on the screen, required method for game
     }
 
+    // Draw the enemy on the screen, required method for game
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
 }
 
-
-
-
+// Now write your own player class
+// This class requires an update(), render() and
+// a handleInput() method.
 class Player {
     constructor(x, y, speed) {
         this.x = x;
@@ -75,6 +74,7 @@ class Player {
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
+
     handleInput(keyPress) {
         if (keyPress === 'left' && this.x > 33) {
             this.x -= 100;
@@ -88,21 +88,21 @@ class Player {
     }
 }
 
-
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var allEnemies = [new Enemy(), new Enemy(), new Enemy(), new Enemy(), new Enemy()];
+let allEnemies = [new Enemy(), new Enemy(), new Enemy(), new Enemy(), new Enemy()];
 
 // Position "y" where the enemies will are created
-var enemyPosition = [50, 140, 220];
-var player = new Player(200, 300, 50);
-var enemy;
+let enemyPosition = [50, 140, 220];
+let player = new Player(200, 300, 50);
+let enemy;
 
 enemyPosition.forEach(function(posY) {
     enemy = new Enemy(0, posY, 100 + Math.floor(Math.random() * 512));
     allEnemies.push(enemy);
 });
+
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
